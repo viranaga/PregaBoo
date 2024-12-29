863ef21760db03ce4ec3a5659eb3ae81e2f8abe2
janithkavishka31 <160833581+janithkavishka31@users.noreply.github.com>
2024-12-29 22:00:09 +0530

    login2 loading

--- app/src/main/java/com/example/pregaboo/views/LoginActivity2.java
+++ app/src/main/java/com/example/pregaboo/views/LoginActivity2.java
@@ -17,10 +17,7 @@ public class LoginActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
 }
